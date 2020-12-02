163398c8de37b2ff5df595e7a8a953ed353394e0
Ashish Suri <aashishhsuri@gmail.com>
2020-12-02 08:36:10 +0530

    uploaded.

--- day2/Program.cs
+++ day2/Program.cs
@@ -4,118 +4,143 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee
+namespace EmployeeApp
 {
-    class Program
+    class Emp
     {
         static void Main(string[] args)
         {
-            Employee1 o1 = new Employee1();
-            Employee1 o2 = new Employee1();
-            Employee1 o3 = new Employee1();
-
-            Console.WriteLine(o1.EMPNO);
-            Console.WriteLine(o2.EMPNO);
-            Console.WriteLine(o3.EMPNO);
-
-            Console.WriteLine(o3.EMPNO);
-            Console.WriteLine(o2.EMPNO);
-            Console.WriteLine(o1.EMPNO);
+            Employee1 e1 = new Employee1("Amol", 12000, 1);
+            Console.WriteLine("Empid: " + e1.EMPNO + ", Name: " + e1.NAME + ", Basic salary: " + e1.BASIC + ", Dept No: " + e1.DEPTNO + ", Net salary: " + e1.GetNetSalary());
             Console.ReadLine();
-        }
-    }
-}
+            Employee1 e2 = new Employee1("Amol", 25000);
+            Console.WriteLine("Empid: " + e2.EMPNO + ", Name: " + e2.NAME + ", Basic salary: " + e2.BASIC + ", Net salary: " + e2.GetNetSalary());
+            Console.ReadLine();
+            Employee1 e3 = new Employee1("", 60);
+            Console.WriteLine("Empid: " + e3.EMPNO + ", Name: " + e3.NAME + ", Basic salary: " + e3.BASIC + ", Net salary: " + e3.GetNetSalary());
+            Console.ReadLine();
+            Employee1 e4 = new Employee1();
+            Console.WriteLine("Empid: " + e4.EMPNO + ", Net salary: " + e4.GetNetSalary());
+            Console.ReadLine();
 
-public class Employee1
-{
-    private string Name;
-    public string NAME
-    {
-        set
-        {
-            if (value != "")
-                Name = value;
-            else
-                Console.WriteLine("No blank names allowed.");
 
-        }
-        get
-        {
-            return Name;
-        }
-    }
+            Console.WriteLine("Empid: " + e4.EMPNO + ", Net salary: " + e4.GetNetSalary());
+            Console.ReadLine();
 
-    static int count = 0;
-    private int EmpNo;
-    public int EMPNO
-    {
-        get
-        {
-            return EmpNo;
+            Console.WriteLine("Empid: " + e3.EMPNO + ", Name: " + e3.NAME + ", Basic salary: " + e3.BASIC + ", Net salary: " + e3.GetNetSalary());
+            Console.ReadLine();
+
+            Console.WriteLine("Empid: " + e2.EMPNO + ", Name: " + e2.NAME + ", Basic salary: " + e2.BASIC + ", Net salary: " + e2.GetNetSalary());
+            Console.ReadLine();
+
+            Console.WriteLine("Empid: " + e1.EMPNO + ", Name: " + e1.NAME + ", Basic salary: " + e1.BASIC + ", Dept No: " + e1.DEPTNO + ", Net salary: " + e1.GetNetSalary());
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 
-    private decimal Basic;
-    public decimal BASIC
+    class Employee1
     {
-        set
+        private string Name;
+        private static int EmpNo;
+        private decimal Basic;
+        private short DeptNo;
+
+        //public static int counter;
+
+        #region Constructors
+        public Employee1()
         {
-            if (20000 <= value && value <= 30000)
-                Basic = value;
-            else
-                Console.WriteLine("Not valid.");
+            EmpNo++;
+            this.EMPNO += EmpNo;
+        }
+        public Employee1(string Name, decimal Basic, short DeptNo)
+        {
+            EmpNo++;
+            this.EMPNO = EmpNo;
+            this.NAME = Name;
+            this.BASIC = Basic;
+            this.DEPTNO = DeptNo;
+        }
+        public Employee1(string Name, decimal Basic)
+        {
+            EmpNo++;
+            this.EMPNO = EmpNo;
+            this.NAME = Name;
+            this.BASIC = Basic;
+        }
+        public Employee1(string Name)
+        {
+            EmpNo++;
+            this.EMPNO = EmpNo;
+            this.NAME = Name;
+        }
+        #endregion
+
+        #region Validations
+        public string NAME
+        {
+            set
+            {
+                if (value == "")
+                    Console.WriteLine("Name should not be empty");
+                else
+                    Name = value;
+
+            }
+            get { return Name; }
+        }
+
+        public int EMPNO
+        {
+            get;
+        }
+        public decimal BASIC
+        {
+            set
+            {
+                if (value >= 10000 && value <= 50000)
+                    Basic = value;
+                else
+                    Console.WriteLine("Enter valid basic");
+            }
+            get { return Basic; }
+        }
+
+        public short DEPTNO
+        {
+            set
+            {
+                if (value >= 0)
+                    DeptNo = value;
+                else
+                    Console.WriteLine("Enter valid Dept no");
+            }
+            get { return DeptNo; }
+        }
+
+
+
+
+
+        #endregion
+
+        public decimal GetNetSalary()
+        {
+            if (BASIC >= 10000 && BASIC <= 50000)
+            {
+                decimal netSal = BASIC + 1000;
+                return netSal;
+            }
+            return BASIC;
+
 
         }
-        get
-        {
-            return Basic;
-        }
-    }
 
-    private short DeptNo;
-    public short DEPTNO
-    {
-        set
-        {
-            if (value > 0)
-                DeptNo = value;
-            else
-                Console.WriteLine("Must be > 0");
 
-        }
-        get
-        {
-            return DeptNo;
-        }
-    }
-    public Employee1()
-    {
-        count++;
-        EmpNo = count;
-    }
-    public Employee1(string NAME, decimal BASIC, short DEPTNO)
-    {
-        count++;
-        EmpNo = count;
-        this.NAME = NAME;
-        this.BASIC = BASIC;
-        this.DEPTNO = DEPTNO;
-    }
-    public Employee1(string NAME, decimal BASIC)
-    {
-        count++;
-        EmpNo = count;
-        this.NAME = NAME;
-        this.BASIC = BASIC;
-    }
-    public Employee1(string NAME)
-    {
-        count++;
-        EmpNo = count;
-        this.NAME = NAME;
-    }
-    public decimal GetNetSalary()
-    {
-        return Basic + (Basic / 10);
     }
 }
